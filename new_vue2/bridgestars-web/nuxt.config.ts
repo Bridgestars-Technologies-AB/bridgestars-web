c9db7edc7bee60eccaf464bc87c050029f471be0
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2023-06-06 11:17:39 +0200

    add nuxt template

---
+++ new_vue2/bridgestars-web/nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    [
+      "@nuxtjs/i18n",
+      {
+        vueI18n: "./localization/i18n.config.ts",
+        locales: [
+          {
+            code: "en",
+            file: "en-US.json",
+            iso: "en-US",
+          },
+          {
+            code: "sv",
+            file: "sv-SE.json",
+            iso: "sv-SE",
+          },
+        ],
+        baseUrl: "https://bridgestars.net",
+
+        langDir: "localization/lang",
+        defaultLocale: "en",
+        lazy: true,
+        /*options*/
+      },
+    ],
+  ],
+});
