379c3e897a3d45d6eaf34293ec261931ee8a2e21
rasmus regnell <rasmuserik2009@gmail.com>
2023-06-07 16:23:39 +0200

    have started to design a basic UI for SignIn form

--- nuxt/components/SignIn.vue
+++ nuxt/components/SignIn.vue
@@ -1,15 +1,26 @@
 <template>
   <form class="signInForm" action="">
-    <input type="text" id="fname" name="fname" /><br />
+    <div class="form-group">
+      <input
+        class="userInp"
+        type="text"
+        placeholder="Username/Email"
+        autofocus
+      />
+    </div>
+    <div class="form-group">
+      <input class="userInp" type="password" placeholder="Password" />
+    </div>
+    <button type="submit">SIGN IN</button>
   </form>
 </template>
 
 <style>
-.signInForm {
+/*.signInForm {
   background-color: gray;
   padding: 1px;
   text-align: left;
   margin: auto;
   display: table;
-}
+}*/
 </style>
