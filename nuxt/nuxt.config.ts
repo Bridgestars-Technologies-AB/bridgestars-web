0daabba99b17d4426092cc7cd65d36db7979dbc2
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2023-06-07 10:32:37 +0200

    add localization to nuxt config

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -8,4 +8,30 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  modules: [
+    [
+      "@nuxtjs/i18n",
+      {
+        vueI18n: "./localization/i18n.config.ts",
+        locales: [
+          {
+            code: "en",
+            file: "en-US.json",
+            iso: "en-US",
+          },
+          {
+            code: "sv",
+            file: "sv-SE.json",
+            iso: "sv-SE",
+          },
+        ],
+        baseUrl: "https://bridgestars.net",
+
+        langDir: "localization/lang",
+        defaultLocale: "en",
+        lazy: true,
+        /*options*/
+      },
+    ],
+  ],
 });
