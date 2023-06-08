bd61e2556f9ecaf767fa7d4b22619dae52c292d7
rasmus regnell <rasmuserik2009@gmail.com>
2023-06-08 14:08:22 +0200

    authform changes

---
+++ nuxt/components/AuthForm.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps(["title", "subtitle", "fields"]);
+defineEmits(["submit"]);
+
+// fields = ["username", "password"];
+</script>
+
+<template>
+  <div>
+    <h1>{{ props.title }}</h1>
+    <h2>{{ props.subtitle }}</h2>
+    <!-- <EmailField v-if="fields.includes('email')" />
+    <UsernameField v-if="fields.includes('username')" />
+    <PasswordField v-if="fields.includes('password')" /> -->
+    <!-- <div class="btn" @click="submit" /> -->
+    <slot></slot>
+  </div>
+</template>
