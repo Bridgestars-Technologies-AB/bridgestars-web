b9e4b950b8bea3569e1cbdb55b44ea44f8d3b5c5
rasmusregnell <rasmuserik2009@gmail.com>
2023-07-20 16:43:05 +0200

    Working on UI, far from done

--- nuxt/components/dash/top-menu.vue
+++ nuxt/components/dash/top-menu.vue
@@ -1,12 +1,27 @@
 <script setup>
-  const common = "w-[24px] h-[24px] bg-dark dark:bg-light hover:bg-dash-accent dark:hover:bg-dash-accent hover:animate-shake"
-  const darkMode = useDarkMode()
+import Settings from "~/pages/dash/settings.vue";
+
+const common =
+  "w-[24px] h-[24px] bg-dark dark:bg-light hover:bg-dash-accent dark:hover:bg-dash-accent hover:animate-shake";
+const darkMode = useDarkMode();
 </script>
 <template>
-  <div class="bg-dash-light-300 dark:bg-dash-dark flex justify-end flex-grow p-3 space-x-4 h-fit">
-    <span :class="`${!darkMode.enabled ? 'i-material-symbols-dark-mode-outline' : 'i-material-symbols-light-mode-outline'}  ${common}`" @click="darkMode.toggle()"></span> 
-    <span :class="`i-material-symbols-notifications-outline ${common}`"></span> 
-    <span :class="`i-material-symbols-settings-outline ${common}`"></span> 
-    <span :class="`i-ic-baseline-account-circle ${common}`"></span> 
+  <div
+    class="bg-dash-light-300 dark:bg-dash-dark flex justify-end flex-grow p-3 space-x-4 h-fit"
+  >
+    <span
+      :class="`${
+        !darkMode.enabled
+          ? 'i-material-symbols-dark-mode-outline'
+          : 'i-material-symbols-light-mode-outline'
+      }  ${common}`"
+      @click="darkMode.toggle()"
+    ></span>
+    <span :class="`i-material-symbols-notifications-outline ${common}`"></span>
+    <span
+      :class="`i-material-symbols-settings-outline ${common}`"
+      @click="navigateTo({ name: 'dash-account-details' })"
+    ></span>
+    <span :class="`i-ic-baseline-account-circle ${common}`"></span>
   </div>
 </template>
---
+++ nuxt/pages/dash/account-details.vue
@@ -0,0 +1,47 @@
+<template>
+  <h1 class="text-[40px] ml[200px]">Account Details</h1>
+  <div class="h-[100%] w-[100%] p-[30px]">
+    <div class="h-[85%] bg-white rounded-xl flex flex-col">
+      <div
+        class="border-black border-2 m-[30px] h-[25%] rounded-xl flex flex-row justify-start content-center"
+      >
+        <!-- Fix responsiveness of picture -->
+        <img
+          class="object-cover object-top w-[120px] aspect-square rounded-full ml-[20px] self-center"
+          src="~/assets/bridgestars/images/castor.jpg"
+        />
+        <div class="mt-[50px] ml-[20px]">
+          <h1 class="text-[20px] leading-[22px] tracking-tighter">
+            Castor Mann
+          </h1>
+          <h6
+            class="mt-[12px] text-[#14C6A4] dark:text-[#14C6a4] font-light tracking-normal"
+          >
+            Bridgestars Premium
+          </h6>
+          <span class="text2">Stockholm, Sweden</span>
+        </div>
+      </div>
+
+      <div class="accountBox h-[30%] w-[45%] rounded-xl">
+        <span class="ml-[5px]">Personal information</span>
+        <div class="flex flex-row justify-between h-[100%]">
+          <div class="ml-[10px] h-[100%] flex flex-col">
+            <span class="">First Name</span>
+            <span class="">Castor</span>
+            <span class="mt-[10px]">Email address</span>
+            <span class="">Something here</span>
+          </div>
+          <div class="flex flex-col">
+            <span>Last Name</span>
+            <span>Mann</span>
+          </div>
+          <button class="self-start">Edit</button>
+        </div>
+      </div>
+      <div
+        class="accountBox rounded-xl flex flex-row justify-start content-center"
+      ></div>
+    </div>
+  </div>
+</template>
