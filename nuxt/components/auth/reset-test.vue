4e93ea1e46c453d1a9f4f5cf3893f88e59bd6764
rasmusregnell <rasmuserik2009@gmail.com>
2023-08-03 12:04:58 +0200

    experimenting, not going so well

--- nuxt/components/auth/reset-test.vue
+++ nuxt/components/auth/reset-test.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup language="javascript" type="text/javascript">
 if (process.client) {
   window.onload = function () {
     var urlParams = {};
@@ -42,150 +42,41 @@ if (process.client) {
 </script>
 
 <template>
-  <h1>Reset Your Password<span id="app"></span></h1>
-  <noscript
-    >We apologize, but resetting your password requires javascript</noscript
+  <auth-form
+    :header="$t('auth:reset.resetPassword')"
+    :title="$t('auth:reset.title')"
+    :subtitle="$t('auth:reset.subtitle2')"
+    @submit="submit"
   >
-  <div class="error" id="error"></div>
-  <form id="form" action="#" method="POST">
-    <label>New Password for <span id="username_label"></span></label>
-    <input name="new_password" type="password" />
-    <input name="utf-8" type="hidden" value="✓" />
-    <input name="username" id="username" type="hidden" />
-    <input name="token" id="token" type="hidden" />
-    <button>Change Password</button>
-  </form>
+    <div class="error" id="error"></div>
+    <form id="form" action="#" method="POST">
+      <label>New Password for <span id="username_label"></span></label>
+      <base-input-field
+        wrapperClass="w-[100%]"
+        :placeholder="$t('auth:placeholder.password')"
+        type="password"
+        id="password"
+        name="new_password"
+      />
+      <base-input-field
+        wrapperClass="w-[100%]"
+        :placeholder="$t('auth:placeholder.confirmPassword')"
+        type="password"
+        id="password-confirm"
+      />
+      <input name="new_password" type="password" />
+      <input name="utf-8" type="hidden" value="✓" />
+      <input name="username" id="username" type="hidden" />
+      <input name="token" id="token" type="hidden" />
+      <base-submit-button
+        wrapperClass="w-[100%] xs:!mt-3 sm:!mt-6"
+        id="submit"
+        type="submit"
+        :text="$t('auth:reset.reset')"
+        @submit="submit"
+      ></base-submit-button>
+    </form>
+  </auth-form>
 </template>
 
-<style>
-h1 {
-  display: block;
-  font: inherit;
-  font-size: 30px;
-  font-weight: 600;
-  height: 30px;
-  line-height: 30px;
-  margin: 45px 0px 45px 0px;
-  padding: 0px 8px 0px 8px;
-}
-
-.error {
-  color: red;
-  padding: 0px 8px 0px 8px;
-  margin: -25px 0px -20px 0px;
-}
-
-body {
-  font-family: "Open Sans", "Helvetica Neue", Helvetica;
-  color: #0067ab;
-  margin: 15px 99px 0px 98px;
-}
-
-label {
-  color: #666666;
-}
-form {
-  margin: 0px 0px 45px 0px;
-  padding: 0px 8px 0px 8px;
-}
-form > * {
-  display: block;
-  margin-top: 25px;
-  margin-bottom: 7px;
-}
-
-button {
-  font-size: 22px;
-  color: white;
-  background: #0067ab;
-  -moz-border-radius: 5px;
-  -webkit-border-radius: 5px;
-  -o-border-radius: 5px;
-  -ms-border-radius: 5px;
-  -khtml-border-radius: 5px;
-  border-radius: 5px;
-  background-image: -webkit-gradient(
-    linear,
-    50% 0,
-    50% 100%,
-    color-stop(0%, #0070ba),
-    color-stop(100%, #00558c)
-  );
-  background-image: -webkit-linear-gradient(#0070ba, #00558c);
-  background-image: -moz-linear-gradient(#0070ba, #00558c);
-  background-image: -o-linear-gradient(#0070ba, #00558c);
-  background-image: -ms-linear-gradient(#0070ba, #00558c);
-  background-image: linear-gradient(#0070ba, #00558c);
-  -moz-box-shadow: inset 0 1px 0 0 #0076c4;
-  -webkit-box-shadow: inset 0 1px 0 0 #0076c4;
-  -o-box-shadow: inset 0 1px 0 0 #0076c4;
-  box-shadow: inset 0 1px 0 0 #0076c4;
-  border: 1px solid #005e9c;
-  padding: 10px 14px;
-  cursor: pointer;
-  outline: none;
-  display: block;
-  font-family: "Helvetica Neue", Helvetica;
-
-  -webkit-box-align: center;
-  text-align: center;
-  box-sizing: border-box;
-  letter-spacing: normal;
-  word-spacing: normal;
-  line-height: normal;
-  text-transform: none;
-  text-indent: 0px;
-  text-shadow: none;
-}
-
-button:hover {
-  background-image: -webkit-gradient(
-    linear,
-    50% 0,
-    50% 100%,
-    color-stop(0%, #0079ca),
-    color-stop(100%, #005e9c)
-  );
-  background-image: -webkit-linear-gradient(#0079ca, #005e9c);
-  background-image: -moz-linear-gradient(#0079ca, #005e9c);
-  background-image: -o-linear-gradient(#0079ca, #005e9c);
-  background-image: -ms-linear-gradient(#0079ca, #005e9c);
-  background-image: linear-gradient(#0079ca, #005e9c);
-  -moz-box-shadow: inset 0 0 0 0 #0076c4;
-  -webkit-box-shadow: inset 0 0 0 0 #0076c4;
-  -o-box-shadow: inset 0 0 0 0 #0076c4;
-  box-shadow: inset 0 0 0 0 #0076c4;
-}
-
-button:active {
-  background-image: -webkit-gradient(
-    linear,
-    50% 0,
-    50% 100%,
-    color-stop(0%, #00395e),
-    color-stop(100%, #005891)
-  );
-  background-image: -webkit-linear-gradient(#00395e, #005891);
-  background-image: -moz-linear-gradient(#00395e, #005891);
-  background-image: -o-linear-gradient(#00395e, #005891);
-  background-image: -ms-linear-gradient(#00395e, #005891);
-  background-image: linear-gradient(#00395e, #005891);
-}
-
-input {
-  color: black;
-  cursor: auto;
-  display: inline-block;
-  font-family: "Helvetica Neue", Helvetica;
-  font-size: 25px;
-  height: 30px;
-  letter-spacing: normal;
-  line-height: normal;
-  margin: 2px 0px 2px 0px;
-  padding: 5px;
-  text-transform: none;
-  vertical-align: baseline;
-  width: 500px;
-  word-spacing: 0px;
-}
-</style>
+<style></style>
