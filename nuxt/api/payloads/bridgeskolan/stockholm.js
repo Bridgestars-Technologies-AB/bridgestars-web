9791742b8f3be5f3b35de1d066117e7956a1cee1
castormann <castormannen@gmail.com>
2023-08-21 15:08:37 +0200

    stockholm sys

--- nuxt/api/payloads/bridgeskolan/stockholm.js
+++ nuxt/api/payloads/bridgeskolan/stockholm.js
@@ -72,3 +72,57 @@ const _1D_3D = {
     }
 }
 
+const _1H_2H = {
+    generate: "1000000",
+    produce: "10",
+    constraints: {
+        "1": "12-21 hcp and 5+ hearts 3- spades 4- diamonds 4- clubs",
+        "4": "3- spades 3+ hearts 5- clubs 5- diamonds 6-10 sph",
+    }
+}
+
+const _1H_3H = {
+    generate: "1000000",
+    produce: "10",
+    constraints: {
+        "1": "12-21 hcp and 5+ hearts 3- spades 4- diamonds 4- clubs",
+        "4": "3- spades 3+ hearts 5- clubs 5- diamonds 11-12 sph",
+    }
+}
+
+const _1S_2S = {
+    generate: "1000000",
+    produce: "10",
+    constraints: {
+        "1": "12-21 hcp and 5+ spades 3- hearts 4- diamonds 4- clubs",
+        "4": "3+ spades 3- hearts 5- clubs 5- diamonds 6-10 sps",
+    }
+}
+
+const _1S_3S = {
+    generate: "1000000",
+    produce: "10",
+    constraints: {
+        "1": "12-21 hcp and 5+ spades 3- hearts 4- diamonds 4- clubs",
+        "4": "3+ spades 3- hearts 5- clubs 5- diamonds 11-12 sps",
+    }
+}
+
+// 1 suit openings with new suit
+const _1C_1D = {
+    generate: "1000000",
+    produce: "10",
+    constraints: {
+        "1": "12-21 hcp and 4- hearts 4- spades 3- diamonds 3+ clubs or longest clubs",
+        "4": "3- spades 3- hearts 4- clubs 4+ diamonds 6+ hcp",
+    }
+}
+
+const _1C_1H = {
+    generate: "1000000",
+    produce: "10",
+    constraints: {
+        "1": "12-21 hcp and 4- hearts 4- spades 3- diamonds 3+ clubs or longest clubs",
+        "4": "3- spades 4+ hearts 4- clubs 4- diamonds 6+ hcp",
+    }
+}
