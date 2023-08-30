dc53ac2d45ac599f059780b266c4bc2ff01fdd25
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2023-08-30 19:33:43 +0200

    bidding history

---
+++ nuxt/components/game/bidding-history.vue
@@ -0,0 +1,5 @@
+<script setup>
+  
+  // for bidding box status display history
+  //const history = [{player:0, suit:0, nbr:0}, {player:1, suit:0, nbr:1}, {player:2, suit:4, nbr:1}]
+</script>
--- nuxt/pages/dash/overview.vue
+++ nuxt/pages/dash/overview.vue
@@ -7,8 +7,10 @@ onMounted(() => {
 
 const suit = ref(0)
 const nbr = ref(0) 
+//const hist = ref([])
 
 function bid(n, s){
+  //hist.value.push({player:0, suit:s, nbr:n})
   console.log("bidding: ", suit.value, nbr.value) //5, 5
 }
 </script>
@@ -16,5 +18,6 @@ function bid(n, s){
   <div class="flex flex-col space-y-1">
     <h1 class="">Overview</h1> 
     <game-bidding-box v-model:suit="suit" v-model:nbr="nbr" @bid="bid"/>
+    <!-- <game-bidding-history :history="hist"/> -->
   </div>
 </template> 
