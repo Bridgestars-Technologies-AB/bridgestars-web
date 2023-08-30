a55ab5eaeba9b565b4968ac489d93d2eab729bcb
rasmusregnell <rasmuserik2009@gmail.com>
2023-08-30 16:42:11 +0200

    auth/already mostly done, but won't detect user already signed in

---
+++ nuxt/components/auth/already.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { User } from "parse";
+
+const query = useRoute().query;
+const toast = useToast();
+const { t } = await loadTranslations("auth");
+const logoutModalOpen = ref(false);
+const hej = "x";
+
+function signOut() {
+  logoutModalOpen.value = true;
+}
+</script>
+
+<template>
+  <auth-form
+    :header="$t('auth:already.already')"
+    :title="$t('auth:already.title', { name: useAuth().get('username') })"
+    :subtitle="$t('auth:already.subtitle')"
+    :subtitle2="$t('auth:already.subtitle2')"
+  >
+    <div>
+      <button
+        @click="navigateTo({ path: '/dash', query })"
+        class="buttons"
+        type="button"
+      >
+        {{ $t("auth:already.dash") }}
+      </button>
+      <button @click="signOut" class="buttons" type="button">
+        {{ $t("auth:already.signOut") }}
+      </button>
+    </div>
+  </auth-form>
+  <base-modal-signout v-model:open="logoutModalOpen" />
+</template>
+
+<style scoped>
+.buttons {
+  @apply border-[1px] border-[#49A3F1] bg-[#49A3F1] m-3 p-2 rounded-lg text-white font-family text-[18px] mt-[-5px] tracking-[2px];
+}
+</style>
