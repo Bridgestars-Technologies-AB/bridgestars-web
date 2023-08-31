f57580b1f9b0ff43877fccc2328a1f8bc6330bf3
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2023-08-31 22:00:57 +0200

    docs functions done

--- nuxt/pages/dev/db-functions.vue
+++ nuxt/pages/dev/db-functions.vue
@@ -1,6 +1,5 @@
 <script setup>
   
-import data from '~/assets/dev/docs-db-functions.json'
 
 const toast = useToast()
 
@@ -13,10 +12,18 @@ function copyUrl(id){
     toast.success(`Copied url to ${id}`)
   })
 }
+const data = ref(null)
+const order = ["signIn", "searchUsers", "getUserInfo", "sendFriendRequest","acceptFriendRequest", "denyFriendRequest", "removeFriend", "redeemVoucher", "generateSubscriptionDashboardLink", "archivePost", "fetchIfUpdated", "signOutFromAllDevices", "assignRole", "assignRoomMod"]
 onMounted(() => {
   //reload data() {
   import ('~/assets/dev/docs-db-functions.json').then((res) => { //reload during development
-    data.value = res
+
+    res.default
+    data.value = []
+    order.forEach((x) => {
+      data.value.push(res.default.filter(y => y['name'] == x)[0])
+    })
+    data.value.push(...res.default.filter(x => !order.includes(x['name'])))
   })
 })
 </script>
@@ -29,27 +36,36 @@ onMounted(() => {
 
   <!--  all classes links -->
       
-      <div class="mx-9 my-9 flex flex-wrap flex-col">
-        <div v-for="table in data" class="flex items-center text-center">
+      <div v-if="data" class="mx-9 my-9 flex flex-wrap flex-col">
+        <div v-for="table in data" class="relative flex items-center text-center">
           <div class="w-full">
             <a :href="`#${table['name']}`" class="xs:text-[14px] md:text-[20px] text-blue font-family2 mr-1">{{table['name']}}</a>
             <span class="i-material-symbols-find-in-page-outline-rounded text-blue text-[14px]"/>
           </div>
+          <div class="absolute w-full flex items-center justify-center">
+          <span class="xs:text-[14px] md:text-[20px] font-family2 opacity-0">{{table['name']}}</span>
+            <div v-for="role in table['roles']" class="ml-[90px] rounded-xl px-[5px] py-[0px] bg-red-400">
+              <span class="text1 text-white font-medium text-[12px]">{{role}}</span>
+            </div>
+          </div>
         </div>
       </div>
 
-    <div class="flex flex-col items-center w-full flex-grow space-y-[50px]">
+    <div v-if="data" class="flex flex-col items-center w-full flex-grow space-y-[80px] pb-[350px]">
       <div v-for="item in data" class="flex flex-col px-3 items-start mt-5 w-full max-w-[1700px]">
-        <div class="flex items-center cursor-pointer mb-1" @click="copyUrl(item['_id'])">
+        <div class="flex items-center cursor-pointer mb-0" @click="copyUrl(item['_id'])">
           <h2 class="sm:text-[25px] xs:text-[16px] font-bold" :id="item['name']">{{item['name']}}</h2>
           <span class="i-material-symbols-link-rounded text-dark text-2xl ml-2"/>
+          <div v-for="role in item['roles']" class="ml-3 rounded-xl px-2 py-1 bg-red-400">
+            <span class="text1 text-white font-medium text-[20px]">{{role}}</span>
+          </div>
         </div>
         <template v-if="item.desc">
-          <div v-for="d in (item.desc+';').split(';')" class="sm:h-[20px] xs:text-[11px]">
+          <div v-for="d in (item.desc).split(';')" class="sm:h-[20px] xs:text-[11px] mt-1">
             <span class="text2 sm:text-[20px] xs:text-[11px] sm:leading-[20px] xs:leading-[11px]">{{d}}</span>
           </div>
         </template>
-        <div class="w-full mt-1 h-[1.5px] opacity-60 bg-dark rounded-full"/>
+        <div class="w-full mt-4 h-[1.5px] opacity-60 bg-dark rounded-full"/>
         <!-- foreach key in item, print nicely -->
         <h4 class="font-bold mt-3 font-family1 sm:text-[20px] xs:text-[11px]">Parameters</h4>
         <table class="w-full">
@@ -80,7 +96,7 @@ onMounted(() => {
           <tr class="">
 
             <td class="!font-normal max-w-[220px]">
-              <template v-if="item.return.type">
+              <template v-if="item.return?.type">
                 <div v-for="d in (item.return.type+';').split(';')">
                   {{d}}
                 </div>
@@ -88,7 +104,7 @@ onMounted(() => {
             </td>
 
             <td>
-              <template v-if="item.return.desc">
+              <template v-if="item.return?.desc">
                 <div v-for="d in (item.return.desc+';').split(';')">
                   {{d}}
                 </div>
