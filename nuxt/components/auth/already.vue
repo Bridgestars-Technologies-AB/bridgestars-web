356cbb75a876d9075dc72893c8ba758e62393e9e
rasmusregnell <rasmuserik2009@gmail.com>
2023-09-01 12:28:19 +0200

    Fixed leading on xs, changed translation to fit on small screens

--- nuxt/components/auth/already.vue
+++ nuxt/components/auth/already.vue
@@ -9,13 +9,14 @@ function signOut() {
   logoutModalOpen.value = true;
 }
 
-//custom action replaces route so that <back> doesn't direct us to "you are already signed in" since we are not anymore signed in 
-const redirect = () => useRouter().replace('/auth/sign-in')
+//custom action replaces route so that <back> doesn't direct us to "you are already signed in" since we are not anymore signed in
+const redirect = () => useRouter().replace("/auth/sign-in");
 
 onMounted(() => {
-  if (!useAuth().authenticated)  // we should never show this page if user is not signed in
-    redirect()
-})
+  if (!useAuth().authenticated)
+    // we should never show this page if user is not signed in
+    redirect();
+});
 </script>
 
 <template>
@@ -38,13 +39,14 @@ onMounted(() => {
       </button>
     </div>
   </auth-form>
-  <base-modal-signout 
-    v-model:open="logoutModalOpen" 
-    :customSignOutAction="redirect" />
+  <base-modal-signout
+    v-model:open="logoutModalOpen"
+    :customSignOutAction="redirect"
+  />
 </template>
 
 <style scoped>
 .buttons {
-  @apply border-[1px] border-[#49A3F1] bg-[#49A3F1] m-3 p-2 rounded-lg text-white font-family text-[18px] mt-[-5px] tracking-[2px];
+  @apply xs:leading-4 border-[1px] border-[#49A3F1] bg-[#49A3F1] m-3 p-2 rounded-lg text-white font-family text-[18px] mt-[-5px] tracking-[2px];
 }
 </style>
