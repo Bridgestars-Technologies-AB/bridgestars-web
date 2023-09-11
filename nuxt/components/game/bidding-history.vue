da19ed398bca9b80bc0237da0a066766b7808610
rasmusregnell <rasmuserik2009@gmail.com>
2023-09-11 16:07:51 +0200

    fixed a couple of things based on feedback

--- nuxt/components/game/bidding-history.vue
+++ nuxt/components/game/bidding-history.vue
@@ -1,4 +1,10 @@
 <script setup>
+/*
+This component displays the bidding history from bidding-box.
+ Props:
+   history: List of {suit, nbr}
+*/
+
 const p = defineProps({
   history: {
     type: [],
@@ -11,16 +17,6 @@ const p = defineProps({
 const suits = ["♣", "♦", "♥", "♠", "NT"];
 const players = ["V", "N", "Ö", "S"];
 
-//Lite ineffektivt att skapa en ny lista varje gång kanske, men det funkar iallafall
-function updateHistoryList() {
-  const historyList = [];
-  for (let i = 0; i < p.history.length - 1; i += 2) {
-    const pair = [p.history[i], p.history[i + 1]];
-    historyList.push(brick(pair[0], pair[1]));
-  }
-  return historyList;
-}
-
 function brick(suit, nbr) {
   return suit === 0 && nbr === 0 ? "PASS" : `${nbr}${suits[suit]}`;
 }
@@ -28,22 +24,35 @@ function brick(suit, nbr) {
 
 <template>
   <div
-    class="flex flex-col items-center w-[300px] h-[100%] bg-dash-dark-300 ml-10 rounded-xl"
+    class="flex flex-col items-center w-[290px] h-[100%] bg-dash-dark-300 ml-10 rounded-xl"
   >
     <div class="w-[100%] flex flex-row">
       <div class="w-1/4 flex flex-row justify-center mt-[16px]" v-for="p in 4">
-        <span class="text1">
+        <span class="text1 text-[32px] mb-[4px]">
           {{ players[p - 1] }}
         </span>
       </div>
     </div>
     <div class="w-[100%] h-[2px] bg-dash-dark-400 mb-[10px]"></div>
-    <div class="flex flex-row flex-wrap w-[100%]">
-      <div
-        class="w-1/4 h-[10%] flex flex-row justify-center mb-[10px]"
-        v-for="e in updateHistoryList()"
-      >
-        <span class="text1 text-[26px]">{{ e }}</span>
+    <div class="flex flex-row justify-center w-full">
+      <div class="flex flex-row flex-wrap w-[97%] space-x-[2px]">
+        <div
+          :class="`w-[24%] mx-[2px] h-[40px] flex flex-row justify-center items-center mb-[10px] rounded-2xl + ${
+            e === 'PASS' ? 'bg-[#0E9F6E]' : 'bg-dash-dark-200'
+          }`"
+          v-for="e in history.map((element) =>
+            brick(element.suit, element.nbr)
+          )"
+        >
+          <span
+            :class="`text1 text-[22px] + ${
+              e === 'PASS' || e.includes('NT')
+                ? 'tracking-[0.5px]'
+                : 'tracking-[2px]'
+            }`"
+            >{{ e }}</span
+          >
+        </div>
       </div>
     </div>
   </div>
--- nuxt/pages/dash/overview.vue
+++ nuxt/pages/dash/overview.vue
@@ -2,23 +2,18 @@
 // initialize components based on data attribute selectors
 onMounted(() => {});
 
-//Lång lista med alla bud, completeHistory ej nästlad const completeHistory = ref([]), prop som skickas till bidding-history
-
 const suit = ref(0);
 const nbr = ref(0);
 const history = ref([]);
 
-function bid(n, s) {
-  history.value.push(n, s);
-  console.log(history.value);
-  //console.log("bidding: ", suit.value, nbr.value); //5, 5
+function bid(s, n) {
+  history.value.push({ suit: s, nbr: n });
 }
 </script>
 <template>
   <div class="flex flex-row space-y-1">
     <h1 class="">Overview</h1>
     <game-bidding-box v-model:suit="suit" v-model:nbr="nbr" @bid="bid" />
-    <!-- <game-bidding-history :history="hist"/> -->
     <game-bidding-history :history="history"></game-bidding-history>
   </div>
 </template>
