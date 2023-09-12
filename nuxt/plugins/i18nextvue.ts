83c03dee12bd91a8f25ac0896c243035c782e27a
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2023-09-12 21:56:25 +0200

    clean up i17nextvue plugin

--- nuxt/plugins/i18nextvue.ts
+++ nuxt/plugins/i18nextvue.ts
@@ -1,12 +1,8 @@
 
 import i18next from 'i18next'
-// import resourcesToBackend from 'i18next-resources-to-backend'
 import I18NextVue from 'i18next-vue'
-
 import universalLanguageDetect from '@unly/universal-language-detector';
-import onLanguageChanged from 'composables/useLanguageChanged';
-// import sv from '~/localization/lang/sv.json'
-// import en from '~/localization/lang/en.json'
+
 const SUPPORTED_LANGUAGES = ['en', 'sv'];
 const FALLBACK_LANG = 'en';
 
@@ -20,34 +16,6 @@ i18next
   defaultNS: 'common',
 })
 
-// i18next.use(LanguageDetector).init({
-//   detection: {
-//     // order and from where user language should be detected
-//     order: ['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
-//
-//     // keys or params to lookup language from
-//     lookupQuerystring: 'lng',
-//     lookupCookie: 'i18next',
-//     lookupLocalStorage: 'i18nextLng',
-//     lookupSessionStorage: 'i18nextLng',
-//     lookupFromPathIndex: 0,
-//     lookupFromSubdomainIndex: 0,
-//
-//     // cache user language on
-//     caches: [],//['localStorage', 'cookie'],
-//     excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
-//
-//     // optional expire and domain for set cookie
-//     cookieMinutes: 10,
-//     cookieDomain: 'myDomain',
-//
-//     // optional htmlTag with lang attribute, the default is:
-//     htmlTag: document.documentElement,
-//
-//     // optional set cookie options, reference:[MDN Set-Cookie docs](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie)
-//     cookieOptions: { path: '/', sameSite: 'strict' },
-//   },
-// });
 
 export default defineNuxtPlugin(nuxtApp => {
   const lang = universalLanguageDetect({
@@ -56,11 +24,11 @@ export default defineNuxtPlugin(nuxtApp => {
     // acceptLanguageHeader: get(req, 'headers.accept-language'), // Optional - Accept-language header will be used when resolving the language on the server side
     // serverCookies: cookies, // Optional - Cookie "i18next" takes precedence over navigator configuration (ex: "i18next: fr"), will only be used on the server side
     errorHandler: (error) => { // Optional - Use you own logger here, Sentry, etc.
-      console.log('Custom error handler:');
+      console.log('Language Detection Error:');
       console.error(error);
     },
   });
-  console.log("Browser language detected: ", lang);
+
   i18next.changeLanguage(lang);
   
   nuxtApp.vueApp.use(I18NextVue, {i18next})
