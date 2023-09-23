a74b78fa31792c2768fc978a7d7074c5fb4b1dae
rasmusregnell <rasmuserik2009@gmail.com>
2023-09-23 17:42:09 +0200

    Added a rule to eslint

--- nuxt/.eslintrc.js
+++ nuxt/.eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
   env: {
     node: true,
+    es2021: true,
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
+    "no-undef": "off",
   },
   globals: {
     $nuxt: true,
