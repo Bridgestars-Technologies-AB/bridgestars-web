7a55310b60e335ed7696e065dd9b9cac1eeb36e5
rasmusregnell <rasmuserik2009@gmail.com>
2023-09-25 18:10:33 +0200

    Fixed eslint to work with vue, ts and js, added some script commands

--- nuxt/.eslintrc.js
+++ nuxt/.eslintrc.js
@@ -3,11 +3,34 @@ module.exports = {
     node: true,
     es2021: true,
   },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    parser: "@typescript-eslint/parser",
+  },
+  plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  overrides: [
+    // Use `overrides` so ESLint can check both JS and TS files.
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+      },
+    },
+  ],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
+    // adding typescript config ex: "@typescript-eslint/no-unused-vars": "error",
     "no-undef": "off",
+    "vue/multi-word-component-names": "off",
   },
   globals: {
     $nuxt: true,
