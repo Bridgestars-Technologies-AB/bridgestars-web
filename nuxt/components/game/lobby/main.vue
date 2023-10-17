649735a2096726898ea1d4ef30055c557b067e19
rasmusregnell <rasmuserik2009@gmail.com>
2023-10-17 13:23:28 +0200

    Styling

--- nuxt/components/game/lobby/main.vue
+++ nuxt/components/game/lobby/main.vue
@@ -4,22 +4,37 @@
   <div
     class="flex flex-col items-center w-[500px] h-[500px] bg-dash-dark-200 rounded-xl"
   >
-    <span class="text1 mt-[60px]"> Spelare </span>
+    <span class="player mt-[60px] !font-semibold"> Spelare </span>
     <game-lobby-button class="mt-[35px]"></game-lobby-button>
-    <div
-      class="flex flex-row justify-center items-center mt-[5px] w-full h-[150px]"
-    >
-      <div class="flex h-full justify-center">
-        <game-lobby-button class="mt-[35px]"></game-lobby-button>
-      </div>
 
-      <div class="bg-[#075B0E] w-[100px] h-[100px] mx-[35px]"></div>
-      <div class="flex h-full justify-center">
-        <game-lobby-button class="mt-[35px]"></game-lobby-button>
+    <div
+      class="flex flex-row justify-center items-center w-full h-[125px] mt-[20px] mb-[20px] space-x-[20px]"
+    >
+      <game-lobby-button></game-lobby-button>
+      <div
+        class="flex items-center justify-center bg-[#075B0E] w-[125px] h-full rounded-xl"
+      >
+        <div class="flex flex-col w-[90%] h-[90%]">
+          <div class="w-full h-1/3 flex justify-center items-start">
+            <span class="player">N</span>
+          </div>
+          <div class="flex flex-row w-full h-1/3 justify-between items-center">
+            <span class="player">W</span> <span class="player">E</span>
+          </div>
+          <div class="w-full h-1/3 flex justify-center items-end">
+            <span class="player">S</span>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="flex items-center">
       <game-lobby-button></game-lobby-button>
     </div>
+
+    <game-lobby-button></game-lobby-button>
   </div>
 </template>
+
+<style scoped>
+.player {
+  @apply text1 !text-white;
+}
+</style>
