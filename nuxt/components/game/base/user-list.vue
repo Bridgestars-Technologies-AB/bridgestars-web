7ef5b4ddc365851f76144d1494ccc9fc3f3fb26f
rasmusregnell <rasmuserik2009@gmail.com>
2023-11-13 11:38:01 +0100

    small fix for user-list

--- nuxt/components/game/base/user-list.vue
+++ nuxt/components/game/base/user-list.vue
@@ -6,19 +6,19 @@ defineProps({
 
 <template>
   <div
-    class="bg-dash-dark-200 w-[300px] h-[300px] flex flex-col items-center space-y-[5px]"
+    class="bg-dash-dark-200 w-[500px] h-[600px] flex flex-col items-center space-y-[5px]"
   >
-    <div class="flex items-center h-[35px] w-full">
-      <span class="text1">{{ title }}</span>
+    <div class="flex items-center h-[35px] w-full ml-[5px]">
+      <span class="text1 text-white font-medium">{{ title }}</span>
     </div>
     <div class="w-[90%] h-[1px] bg-dash-dark-400 self-center"></div>
-    <div class="w-[90%] h-[220px] border overflow-y-scroll space-y-[2px]">
+    <div class="w-[90%] h-[220px] overflow-y-scroll space-y-[2px]">
       <game-lobby-player v-for="i in 10" :key="i"></game-lobby-player>
     </div>
     <input
       type="text"
       placeholder="Filter..."
-      class="bg-dash-dark-100 rounded-lg w-[90%] h-[30px] text-white text-[10px]"
+      class="bg-dash-dark-100 rounded-lg w-[90%] h-[30px] text-white text-[10px] p-[4px]"
     />
   </div>
 </template>
