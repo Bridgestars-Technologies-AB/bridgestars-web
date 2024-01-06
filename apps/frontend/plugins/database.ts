9afc84a1d567c3c84489167cea5059b2cae18614
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2024-01-06 18:45:51 +0100

    use testing db on dev

--- apps/frontend/plugins/database.ts
+++ apps/frontend/plugins/database.ts
@@ -1,5 +1,5 @@
 import { setServerURL } from "bridgestars-db-client";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  //if (process.dev) setServerURL("http://localhost:1337/rest");
+  if (process.dev) setServerURL("http://localhost:1337/rest");
 });
