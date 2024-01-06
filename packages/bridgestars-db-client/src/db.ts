ba82621daa8490bd06d4ff818688f01a2021292a
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2024-01-06 17:55:12 +0100

    add backend server host config

--- packages/bridgestars-db-client/src/db.ts
+++ packages/bridgestars-db-client/src/db.ts
@@ -1,6 +1,6 @@
 import { initialize } from 'parse-sdk-ts';
 
 initialize(
- "https://bridgestars-backend.fly.dev/rest",
-"76SGU2S6Sixv78YNTLfXVoRhq794D8o2"
+ "https://bridgestars-demo-backend.fly.dev/rest",
+ "v#uoFXM#7n5Yk^7ky8@bBX^^!3cZs3EYF@6DU"
 )
