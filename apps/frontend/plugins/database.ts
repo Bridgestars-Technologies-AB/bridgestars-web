6de3e3e3094548e0eac58bc7bd88961776a6744e
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2024-01-09 17:19:35 +0100

    database connection cleanup

--- apps/frontend/plugins/database.ts
+++ apps/frontend/plugins/database.ts
@@ -1,7 +1,7 @@
 import { initialize } from "bridgestars-db-client";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  if (process.dev) {
+  if (process.dev && process.local_server) {
     initialize(
       "http://localhost:1337/rest",
       "abc123"
