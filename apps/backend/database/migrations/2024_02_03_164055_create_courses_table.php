da6ce56a9943aa8c685fa714c4b5ca49e21fcd1b
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2024-02-03 18:44:10 +0100

    add models, factories, controllers

---
+++ apps/backend/database/migrations/2024_02_03_164055_create_courses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->publicId();
+            $table->nonNullableTimestamps();
+
+            $table->string('name', 100);
+            $table->string('description', 200);
+            $table->integerIncrements('course_nbr');
+            $table->string('color', 11); //255,255,255
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
