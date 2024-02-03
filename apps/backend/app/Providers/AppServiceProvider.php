a6dfa2d4d61809cefcca70fa82aa99cc3478e6f0
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2024-02-03 18:44:10 +0100

    add models, factories, controllers

--- apps/backend/app/Providers/AppServiceProvider.php
+++ apps/backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +25,22 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Model::shouldBeStrict();
+        Date::use(CarbonImmutable::class);
+
+/*        Relation::enforceMorphMap([
+            'user' => User::class,
+        ]);*/
+
+        JsonResource::withoutWrapping();
+
+        Blueprint::macro('publicId', function (string $column = 'public_id', int $length = 12) {
+            $this->string($column, $length)->unique(); // @phpstan-ignore-line
+        });
+
+        Blueprint::macro('nonNullableTimestamps', function () {
+            $this->timestamp('created_at'); // @phpstan-ignore-line
+            $this->timestamp('updated_at'); // @phpstan-ignore-line
+        });
     }
 }
