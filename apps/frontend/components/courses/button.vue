cf3f6f1508510fa97ca1cb5446df4ee37a209ae0
rasmusregnell <rasmuserik2009@gmail.com>
2024-02-08 13:06:31 +0100

    Started on changing frontend to ease the integration with backend

--- apps/frontend/components/courses/button.vue
+++ apps/frontend/components/courses/button.vue
@@ -1,12 +1,12 @@
 <!-- Component for button used on course pages for navigating the dashboard -->
 <script setup>
-defineProps({ text: String, url: String });
+defineProps({ text: String, url: String, params: Number });
 </script>
 
 <template>
   <button
     class="w-[80px] h-[20px] rounded-2xl m-1 text-[12px] bg-[#168f7c] text-white font-semibold"
-    @click="() => navigateTo({ name: 'dash-' + url })"
+    @click="() => navigateTo({ path: `${params}` })"
   >
     {{ text }}
   </button>
---
+++ apps/frontend/pages/dash/my-courses/[id].vue
@@ -0,0 +1,48 @@
+<script setup>
+const route = useRoute();
+console.log(route.params.id);
+
+//axios.get(“/prefix/courses/id”) -> response.data
+const response = {
+  name: "BridgeA",
+  description: "This is a course about BridgeA",
+  chapters: [
+    {
+      name: "Kapitel 5",
+      //paid,       true/false
+      //next_problem_id to get to the bidding-problem in question
+    },
+    {
+      name: "Kapitel 6",
+    },
+    {
+      name: "Kapitel 7",
+    },
+  ],
+};
+</script>
+
+<template>
+  <div class="flex flex-col items-center space-y-5 w-full h-full">
+    <span class="text-3xl text-dark dark:text-white font-semibold">{{
+      response.name
+    }}</span>
+
+    <div class="w-full flex flex-row justify-center">
+      <div
+        :class="
+          response.chapters.length > 1
+            ? 'grid grid-cols-3 grid-flow-row gap-3'
+            : 'flex flex-row space-x-3'
+        "
+      >
+        <courses-bidding-problems-template
+          v-for="chapter in response.chapters"
+          :key="chapter.name"
+          :header="chapter.name"
+          url="xxx"
+        ></courses-bidding-problems-template>
+      </div>
+    </div>
+  </div>
+</template>
