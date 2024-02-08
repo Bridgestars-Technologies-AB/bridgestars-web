ec942b2be5ca618f0d9f9c939833bd226cf89c40
rasmusregnell <rasmuserik2009@gmail.com>
2024-02-08 21:28:54 +0100

    added some methods to Bid.ts

--- apps/frontend/composables/biddingClasses/Bid.ts
+++ apps/frontend/composables/biddingClasses/Bid.ts
@@ -5,6 +5,50 @@ class Bid {
   //public explanation: string;
 
   public constructor(suit: number, rank: number) {
-    this.shortName = toShortName(suit, rank);
+    this.suit = suit;
+    this.rank = rank;
+    this.shortName = this.getShortName();
+  }
+
+  public is(str: string): boolean {
+    switch (str) {
+      case "PASS":
+        return this.suit === 0 && this.rank === 0;
+      case "NT":
+        return this.suit === 4;
+      case "DOUBLE":
+        //check if the bid is a double
+        return false;
+      case "REDOUBLE":
+        //check if the bid is a redouble
+        return false;
+      default:
+        return false;
+    }
+  }
+
+  public getShortName(): string {
+    if (this.rank === 0 && this.suit === 0) {
+      return "PASS";
+    }
+    if (this.suit === 4) {
+      return this.rank + "NT";
+    }
+    return "";
+  }
+
+  public tailwindSuitSymbol(suit: number): string {
+    switch (suit) {
+      case 1:
+        return "i-fluent-emoji-high-contrast-club-suit !text-clubs";
+      case 2:
+        return "i-fluent-emoji-high-contrast-diamond-suit !text-diamonds";
+      case 3:
+        return "i-fluent-emoji-high-contrast-heart-suit !text-hearts";
+      case 4:
+        return "i-fluent-emoji-high-contrast-spade-suit !text-spades";
+      default:
+        return "";
+    }
   }
 }
