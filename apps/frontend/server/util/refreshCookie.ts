0d8c852e73e28a63ce986d7417f8818eee7ad750
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2024-02-09 21:14:51 +0100

    remove server api routes

--- apps/frontend/server/util/refreshCookie.ts
+++
@@ -1,7 +0,0 @@
-export default function refreshCookie(event){
-  const cookies = parseCookies(event);
-  if (cookies["XSRF-TOKEN"]) {
-  }
-    console.log(JSON.stringify((cookies)))
-  return cookies["XSRF-TOKEN"] 
-}
