dacf385ff990a856e8e0dfd203a2ff3893ee2547
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2024-02-10 19:43:44 +0100

    fix build?

--- apps/frontend/plugins/database.ts
+++
@@ -1,8 +0,0 @@
-import { initialize } from "bridgestars-db-client";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  const config = useRuntimeConfig();
-  if (process.dev && !config.public.PROD_SERVER) {
-    initialize("http://localhost:1337/rest", "abc123");
-  }
-});
