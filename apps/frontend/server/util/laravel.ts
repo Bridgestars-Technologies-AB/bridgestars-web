e80b1c73b1c9553090ab1dc0c9d407bb17b5cdf7
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2024-02-17 08:41:16 +0100

    fix types in laravel api file

--- apps/frontend/server/util/laravel.ts
+++ apps/frontend/server/util/laravel.ts
@@ -1,20 +1,24 @@
-export async function post(event, path) {
+import {H3Event} from "h3";
+
+export async function post(event: H3Event<Request>, path: string) {
   return $fetch(process.env.BACKEND_URL + path, {
     method: "POST",
     body: await readBody(event),
+    // @ts-ignore
     headers: {
       "Content-Type": "application/json",
-      accept: "application/json",
-      Authorization: "Bearer " + parseCookies(event)["auth"],
+      "Accept": "application/json",
+      "Authorization": "Bearer " + parseCookies(event)["auth"],
       "X-Api-Key": process.env.API_KEY,
     },
   });
 }
 
-export function get(event, path) {
+export function get(event: H3Event<Request>, path: string) {
   return $fetch(process.env.BACKEND_URL + path, {
     method: "GET",
     //body: await readBody(event),
+    // @ts-ignore
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json",
